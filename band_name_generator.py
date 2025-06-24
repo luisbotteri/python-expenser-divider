94188bad5f2d35efbdf2ecad9179333df0aafb0d
Luis Botteri <luisbotteri30@hotmail.com>
2025-06-24 18:58:37 -0300

    This is a simple and funny code to generate a band name using your name and your pet's name!

---
+++ band_name_generator.py
@@ -0,0 +1,6 @@
+name = input("Hello! What's your name? ")
+print(f"Nice to meet you {name}!\n"
+      "We're going to use your name as part of your band name :D")
+pet_name = input("Please, now write your pet's name: ")
+band_name = pet_name + " " + "of" " " + name
+print(f"That's a cool name! With these information your band name will be {band_name}")
